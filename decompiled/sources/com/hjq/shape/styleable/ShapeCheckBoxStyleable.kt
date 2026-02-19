0d17ce7eb3b7f9f715e9ec800bfebd680c596111
hossien <h.rajaei@vasl.ir>
2026-02-20 01:19:48 +0330

    Convert compound shape styleables to Kotlin

---
+++ decompiled/sources/com/hjq/shape/styleable/ShapeCheckBoxStyleable.kt
@@ -0,0 +1,118 @@
+package com.hjq.shape.styleable;
+
+import com.hjq.shape.R;
+class ShapeCheckBoxStyleable : IShapeDrawableStyleable, ITextColorStyleable, ICompoundButtonStyleable {
+
+    override fun getShapeTypeStyleable(): Int = R.styleable.ShapeCheckBox_shape
+
+    override fun getShapeWidthStyleable(): Int = R.styleable.ShapeCheckBox_shape_width
+
+    override fun getShapeHeightStyleable(): Int = R.styleable.ShapeCheckBox_shape_height
+
+    override fun getSolidColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_solidColor
+
+    override fun getSolidPressedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_solidPressedColor
+
+    override fun getSolidCheckedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_solidCheckedColor
+
+    override fun getSolidDisabledColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_solidDisabledColor
+
+    override fun getSolidFocusedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_solidFocusedColor
+
+    override fun getSolidSelectedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_solidSelectedColor
+
+    override fun getRadiusStyleable(): Int = R.styleable.ShapeCheckBox_shape_radius
+
+    override fun getTopLeftRadiusStyleable(): Int = R.styleable.ShapeCheckBox_shape_topLeftRadius
+
+    override fun getTopRightRadiusStyleable(): Int = R.styleable.ShapeCheckBox_shape_topRightRadius
+
+    override fun getBottomLeftRadiusStyleable(): Int = R.styleable.ShapeCheckBox_shape_bottomLeftRadius
+
+    override fun getBottomRightRadiusStyleable(): Int = R.styleable.ShapeCheckBox_shape_bottomRightRadius
+
+    override fun getStartColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_startColor
+
+    override fun getCenterColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_centerColor
+
+    override fun getEndColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_endColor
+
+    override fun getUseLevelStyleable(): Int = R.styleable.ShapeCheckBox_shape_useLevel
+
+    override fun getAngleStyleable(): Int = R.styleable.ShapeCheckBox_shape_angle
+
+    override fun getGradientTypeStyleable(): Int = R.styleable.ShapeCheckBox_shape_gradientType
+
+    override fun getCenterXStyleable(): Int = R.styleable.ShapeCheckBox_shape_centerX
+
+    override fun getCenterYStyleable(): Int = R.styleable.ShapeCheckBox_shape_centerY
+
+    override fun getGradientRadiusStyleable(): Int = R.styleable.ShapeCheckBox_shape_gradientRadius
+
+    override fun getStrokeColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_strokeColor
+
+    override fun getStrokePressedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_strokePressedColor
+
+    override fun getStrokeCheckedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_strokeCheckedColor
+
+    override fun getStrokeDisabledColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_strokeDisabledColor
+
+    override fun getStrokeFocusedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_strokeFocusedColor
+
+    override fun getStrokeSelectedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_strokeSelectedColor
+
+    override fun getStrokeWidthStyleable(): Int = R.styleable.ShapeCheckBox_shape_strokeWidth
+
+    override fun getDashWidthStyleable(): Int = R.styleable.ShapeCheckBox_shape_dashWidth
+
+    override fun getDashGapStyleable(): Int = R.styleable.ShapeCheckBox_shape_dashGap
+
+    override fun getInnerRadiusStyleable(): Int = R.styleable.ShapeCheckBox_shape_innerRadius
+
+    override fun getInnerRadiusRatioStyleable(): Int = R.styleable.ShapeCheckBox_shape_innerRadiusRatio
+
+    override fun getThicknessStyleable(): Int = R.styleable.ShapeCheckBox_shape_thickness
+
+    override fun getThicknessRatioStyleable(): Int = R.styleable.ShapeCheckBox_shape_thicknessRatio
+
+    override fun getShadowSizeStyleable(): Int = R.styleable.ShapeCheckBox_shape_shadowSize
+
+    override fun getShadowColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_shadowColor
+
+    override fun getShadowOffsetXStyleable(): Int = R.styleable.ShapeCheckBox_shape_shadowOffsetX
+
+    override fun getShadowOffsetYStyleable(): Int = R.styleable.ShapeCheckBox_shape_shadowOffsetY
+
+    override fun getTextColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textColor
+
+    override fun getTextPressedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textPressedColor
+
+    override fun getTextCheckedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textCheckedColor
+
+    override fun getTextDisabledColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textDisabledColor
+
+    override fun getTextFocusedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textFocusedColor
+
+    override fun getTextSelectedColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textSelectedColor
+
+    override fun getTextStartColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textStartColor
+
+    override fun getTextCenterColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textCenterColor
+
+    override fun getTextEndColorStyleable(): Int = R.styleable.ShapeCheckBox_shape_textEndColor
+
+    override fun getTextGradientOrientationStyleable(): Int = R.styleable.ShapeCheckBox_shape_textGradientOrientation
+
+    override fun getButtonDrawableStyleable(): Int = R.styleable.ShapeCheckBox_shape_buttonDrawable
+
+    override fun getButtonPressedDrawableStyleable(): Int = R.styleable.ShapeCheckBox_shape_buttonPressedDrawable
+
+    override fun getButtonCheckedDrawableStyleable(): Int = R.styleable.ShapeCheckBox_shape_buttonCheckedDrawable
+
+    override fun getButtonDisabledDrawableStyleable(): Int = R.styleable.ShapeCheckBox_shape_buttonDisabledDrawable
+
+    override fun getButtonFocusedDrawableStyleable(): Int = R.styleable.ShapeCheckBox_shape_buttonFocusedDrawable
+
+    override fun getButtonSelectedDrawableStyleable(): Int = R.styleable.ShapeCheckBox_shape_buttonSelectedDrawable
+
+}
---
+++ decompiled/sources/com/hjq/shape/styleable/ShapeRadioButtonStyleable.kt
@@ -0,0 +1,118 @@
+package com.hjq.shape.styleable;
+
+import com.hjq.shape.R;
+class ShapeRadioButtonStyleable : IShapeDrawableStyleable, ITextColorStyleable, ICompoundButtonStyleable {
+
+    override fun getShapeTypeStyleable(): Int = R.styleable.ShapeRadioButton_shape
+
+    override fun getShapeWidthStyleable(): Int = R.styleable.ShapeRadioButton_shape_width
+
+    override fun getShapeHeightStyleable(): Int = R.styleable.ShapeRadioButton_shape_height
+
+    override fun getSolidColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_solidColor
+
+    override fun getSolidPressedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_solidPressedColor
+
+    override fun getSolidCheckedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_solidCheckedColor
+
+    override fun getSolidDisabledColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_solidDisabledColor
+
+    override fun getSolidFocusedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_solidFocusedColor
+
+    override fun getSolidSelectedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_solidSelectedColor
+
+    override fun getRadiusStyleable(): Int = R.styleable.ShapeRadioButton_shape_radius
+
+    override fun getTopLeftRadiusStyleable(): Int = R.styleable.ShapeRadioButton_shape_topLeftRadius
+
+    override fun getTopRightRadiusStyleable(): Int = R.styleable.ShapeRadioButton_shape_topRightRadius
+
+    override fun getBottomLeftRadiusStyleable(): Int = R.styleable.ShapeRadioButton_shape_bottomLeftRadius
+
+    override fun getBottomRightRadiusStyleable(): Int = R.styleable.ShapeRadioButton_shape_bottomRightRadius
+
+    override fun getStartColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_startColor
+
+    override fun getCenterColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_centerColor
+
+    override fun getEndColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_endColor
+
+    override fun getUseLevelStyleable(): Int = R.styleable.ShapeRadioButton_shape_useLevel
+
+    override fun getAngleStyleable(): Int = R.styleable.ShapeRadioButton_shape_angle
+
+    override fun getGradientTypeStyleable(): Int = R.styleable.ShapeRadioButton_shape_gradientType
+
+    override fun getCenterXStyleable(): Int = R.styleable.ShapeRadioButton_shape_centerX
+
+    override fun getCenterYStyleable(): Int = R.styleable.ShapeRadioButton_shape_centerY
+
+    override fun getGradientRadiusStyleable(): Int = R.styleable.ShapeRadioButton_shape_gradientRadius
+
+    override fun getStrokeColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_strokeColor
+
+    override fun getStrokePressedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_strokePressedColor
+
+    override fun getStrokeCheckedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_strokeCheckedColor
+
+    override fun getStrokeDisabledColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_strokeDisabledColor
+
+    override fun getStrokeFocusedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_strokeFocusedColor
+
+    override fun getStrokeSelectedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_strokeSelectedColor
+
+    override fun getStrokeWidthStyleable(): Int = R.styleable.ShapeRadioButton_shape_strokeWidth
+
+    override fun getDashWidthStyleable(): Int = R.styleable.ShapeRadioButton_shape_dashWidth
+
+    override fun getDashGapStyleable(): Int = R.styleable.ShapeRadioButton_shape_dashGap
+
+    override fun getInnerRadiusStyleable(): Int = R.styleable.ShapeRadioButton_shape_innerRadius
+
+    override fun getInnerRadiusRatioStyleable(): Int = R.styleable.ShapeRadioButton_shape_innerRadiusRatio
+
+    override fun getThicknessStyleable(): Int = R.styleable.ShapeRadioButton_shape_thickness
+
+    override fun getThicknessRatioStyleable(): Int = R.styleable.ShapeRadioButton_shape_thicknessRatio
+
+    override fun getShadowSizeStyleable(): Int = R.styleable.ShapeRadioButton_shape_shadowSize
+
+    override fun getShadowColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_shadowColor
+
+    override fun getShadowOffsetXStyleable(): Int = R.styleable.ShapeRadioButton_shape_shadowOffsetX
+
+    override fun getShadowOffsetYStyleable(): Int = R.styleable.ShapeRadioButton_shape_shadowOffsetY
+
+    override fun getTextColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textColor
+
+    override fun getTextPressedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textPressedColor
+
+    override fun getTextCheckedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textCheckedColor
+
+    override fun getTextDisabledColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textDisabledColor
+
+    override fun getTextFocusedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textFocusedColor
+
+    override fun getTextSelectedColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textSelectedColor
+
+    override fun getTextStartColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textStartColor
+
+    override fun getTextCenterColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textCenterColor
+
+    override fun getTextEndColorStyleable(): Int = R.styleable.ShapeRadioButton_shape_textEndColor
+
+    override fun getTextGradientOrientationStyleable(): Int = R.styleable.ShapeRadioButton_shape_textGradientOrientation
+
+    override fun getButtonDrawableStyleable(): Int = R.styleable.ShapeRadioButton_shape_buttonDrawable
+
+    override fun getButtonPressedDrawableStyleable(): Int = R.styleable.ShapeRadioButton_shape_buttonPressedDrawable
+
+    override fun getButtonCheckedDrawableStyleable(): Int = R.styleable.ShapeRadioButton_shape_buttonCheckedDrawable
+
+    override fun getButtonDisabledDrawableStyleable(): Int = R.styleable.ShapeRadioButton_shape_buttonDisabledDrawable
+
+    override fun getButtonFocusedDrawableStyleable(): Int = R.styleable.ShapeRadioButton_shape_buttonFocusedDrawable
+
+    override fun getButtonSelectedDrawableStyleable(): Int = R.styleable.ShapeRadioButton_shape_buttonSelectedDrawable
+
+}
