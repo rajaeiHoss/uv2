397d6e30f7242175009f9ba66e80c2723559fd5e
hossien <h.rajaei@vasl.ir>
2026-02-19 22:59:00 +0330

    Convert title bar R constants to Kotlin

---
+++ decompiled/sources/com/hjq/bar/R.kt
@@ -0,0 +1,165 @@
+package com.hjq.bar
+
+class R private constructor() {
+    class attr private constructor() {
+        companion object {
+            const val barStyle: Int = 2130968673
+            const val childPaddingHorizontal: Int = 2130968800
+            const val childPaddingVertical: Int = 2130968801
+            const val leftBackground: Int = 2130969215
+            const val leftIcon: Int = 2130969216
+            const val leftIconGravity: Int = 2130969217
+            const val leftIconHeight: Int = 2130969218
+            const val leftIconPadding: Int = 2130969219
+            const val leftIconTint: Int = 2130969220
+            const val leftIconWidth: Int = 2130969221
+            const val leftTitle: Int = 2130969222
+            const val leftTitleColor: Int = 2130969223
+            const val leftTitleSize: Int = 2130969224
+            const val leftTitleStyle: Int = 2130969225
+            const val lineDrawable: Int = 2130969229
+            const val lineSize: Int = 2130969231
+            const val lineVisible: Int = 2130969233
+            const val rightBackground: Int = 2130969452
+            const val rightIcon: Int = 2130969453
+            const val rightIconGravity: Int = 2130969454
+            const val rightIconHeight: Int = 2130969455
+            const val rightIconPadding: Int = 2130969456
+            const val rightIconTint: Int = 2130969457
+            const val rightIconWidth: Int = 2130969458
+            const val rightTitle: Int = 2130969459
+            const val rightTitleColor: Int = 2130969460
+            const val rightTitleSize: Int = 2130969461
+            const val rightTitleStyle: Int = 2130969462
+            const val title: Int = 2130969696
+            const val titleColor: Int = 2130969699
+            const val titleGravity: Int = 2130969701
+            const val titleIcon: Int = 2130969702
+            const val titleIconGravity: Int = 2130969703
+            const val titleIconHeight: Int = 2130969704
+            const val titleIconPadding: Int = 2130969705
+            const val titleIconTint: Int = 2130969706
+            const val titleIconWidth: Int = 2130969707
+            const val titleSize: Int = 2130969714
+            const val titleStyle: Int = 2130969715
+        }
+    }
+
+    class drawable private constructor() {
+        companion object {
+            const val bar_arrows_left_black: Int = 2131230820
+            const val bar_arrows_left_white: Int = 2131230821
+            const val bar_drawable_placeholder: Int = 2131230822
+        }
+    }
+
+    class id private constructor() {
+        companion object {
+            const val light: Int = 2131362512
+            const val night: Int = 2131362825
+            const val ripple: Int = 2131362989
+            const val transparent: Int = 2131363236
+        }
+    }
+
+    class string private constructor() {
+        companion object {
+            const val bar_string_placeholder: Int = 2131755077
+        }
+    }
+
+    class style private constructor() {
+        companion object {
+            const val TitleBarStyle: Int = 2131821110
+        }
+    }
+
+    class styleable private constructor() {
+        companion object {
+            @JvmField
+            val TitleBar: IntArray = intArrayOf(
+                16842964,
+                com.zycs.UView.R.attr.barStyle,
+                com.zycs.UView.R.attr.childPaddingHorizontal,
+                com.zycs.UView.R.attr.childPaddingVertical,
+                com.zycs.UView.R.attr.leftBackground,
+                com.zycs.UView.R.attr.leftIcon,
+                com.zycs.UView.R.attr.leftIconGravity,
+                com.zycs.UView.R.attr.leftIconHeight,
+                com.zycs.UView.R.attr.leftIconPadding,
+                com.zycs.UView.R.attr.leftIconTint,
+                com.zycs.UView.R.attr.leftIconWidth,
+                com.zycs.UView.R.attr.leftTitle,
+                com.zycs.UView.R.attr.leftTitleColor,
+                com.zycs.UView.R.attr.leftTitleSize,
+                com.zycs.UView.R.attr.leftTitleStyle,
+                com.zycs.UView.R.attr.lineDrawable,
+                com.zycs.UView.R.attr.lineSize,
+                com.zycs.UView.R.attr.lineVisible,
+                com.zycs.UView.R.attr.rightBackground,
+                com.zycs.UView.R.attr.rightIcon,
+                com.zycs.UView.R.attr.rightIconGravity,
+                com.zycs.UView.R.attr.rightIconHeight,
+                com.zycs.UView.R.attr.rightIconPadding,
+                com.zycs.UView.R.attr.rightIconTint,
+                com.zycs.UView.R.attr.rightIconWidth,
+                com.zycs.UView.R.attr.rightTitle,
+                com.zycs.UView.R.attr.rightTitleColor,
+                com.zycs.UView.R.attr.rightTitleSize,
+                com.zycs.UView.R.attr.rightTitleStyle,
+                com.zycs.UView.R.attr.title,
+                com.zycs.UView.R.attr.titleColor,
+                com.zycs.UView.R.attr.titleGravity,
+                com.zycs.UView.R.attr.titleIcon,
+                com.zycs.UView.R.attr.titleIconGravity,
+                com.zycs.UView.R.attr.titleIconHeight,
+                com.zycs.UView.R.attr.titleIconPadding,
+                com.zycs.UView.R.attr.titleIconTint,
+                com.zycs.UView.R.attr.titleIconWidth,
+                com.zycs.UView.R.attr.titleSize,
+                com.zycs.UView.R.attr.titleStyle
+            )
+
+            const val TitleBar_android_background: Int = 0
+            const val TitleBar_barStyle: Int = 1
+            const val TitleBar_childPaddingHorizontal: Int = 2
+            const val TitleBar_childPaddingVertical: Int = 3
+            const val TitleBar_leftBackground: Int = 4
+            const val TitleBar_leftIcon: Int = 5
+            const val TitleBar_leftIconGravity: Int = 6
+            const val TitleBar_leftIconHeight: Int = 7
+            const val TitleBar_leftIconPadding: Int = 8
+            const val TitleBar_leftIconTint: Int = 9
+            const val TitleBar_leftIconWidth: Int = 10
+            const val TitleBar_leftTitle: Int = 11
+            const val TitleBar_leftTitleColor: Int = 12
+            const val TitleBar_leftTitleSize: Int = 13
+            const val TitleBar_leftTitleStyle: Int = 14
+            const val TitleBar_lineDrawable: Int = 15
+            const val TitleBar_lineSize: Int = 16
+            const val TitleBar_lineVisible: Int = 17
+            const val TitleBar_rightBackground: Int = 18
+            const val TitleBar_rightIcon: Int = 19
+            const val TitleBar_rightIconGravity: Int = 20
+            const val TitleBar_rightIconHeight: Int = 21
+            const val TitleBar_rightIconPadding: Int = 22
+            const val TitleBar_rightIconTint: Int = 23
+            const val TitleBar_rightIconWidth: Int = 24
+            const val TitleBar_rightTitle: Int = 25
+            const val TitleBar_rightTitleColor: Int = 26
+            const val TitleBar_rightTitleSize: Int = 27
+            const val TitleBar_rightTitleStyle: Int = 28
+            const val TitleBar_title: Int = 29
+            const val TitleBar_titleColor: Int = 30
+            const val TitleBar_titleGravity: Int = 31
+            const val TitleBar_titleIcon: Int = 32
+            const val TitleBar_titleIconGravity: Int = 33
+            const val TitleBar_titleIconHeight: Int = 34
+            const val TitleBar_titleIconPadding: Int = 35
+            const val TitleBar_titleIconTint: Int = 36
+            const val TitleBar_titleIconWidth: Int = 37
+            const val TitleBar_titleSize: Int = 38
+            const val TitleBar_titleStyle: Int = 39
+        }
+    }
+}
