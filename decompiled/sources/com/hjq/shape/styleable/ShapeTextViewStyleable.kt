caea65b69393a5e95a9a3d2363a99ac78ba5453b
hossien <h.rajaei@vasl.ir>
2026-02-20 01:18:51 +0330

    Convert text-capable shape styleables to Kotlin

---
+++ decompiled/sources/com/hjq/shape/styleable/ShapeTextViewStyleable.kt
@@ -0,0 +1,100 @@
+package com.hjq.shape.styleable;
+
+import com.hjq.shape.R;
+class ShapeTextViewStyleable : IShapeDrawableStyleable, ITextColorStyleable {
+
+    override fun getShapeTypeStyleable(): Int = R.styleable.ShapeTextView_shape
+
+    override fun getShapeWidthStyleable(): Int = R.styleable.ShapeTextView_shape_width
+
+    override fun getShapeHeightStyleable(): Int = R.styleable.ShapeTextView_shape_height
+
+    override fun getSolidColorStyleable(): Int = R.styleable.ShapeTextView_shape_solidColor
+
+    override fun getSolidPressedColorStyleable(): Int = R.styleable.ShapeTextView_shape_solidPressedColor
+
+    override fun getSolidDisabledColorStyleable(): Int = R.styleable.ShapeTextView_shape_solidDisabledColor
+
+    override fun getSolidFocusedColorStyleable(): Int = R.styleable.ShapeTextView_shape_solidFocusedColor
+
+    override fun getSolidSelectedColorStyleable(): Int = R.styleable.ShapeTextView_shape_solidSelectedColor
+
+    override fun getRadiusStyleable(): Int = R.styleable.ShapeTextView_shape_radius
+
+    override fun getTopLeftRadiusStyleable(): Int = R.styleable.ShapeTextView_shape_topLeftRadius
+
+    override fun getTopRightRadiusStyleable(): Int = R.styleable.ShapeTextView_shape_topRightRadius
+
+    override fun getBottomLeftRadiusStyleable(): Int = R.styleable.ShapeTextView_shape_bottomLeftRadius
+
+    override fun getBottomRightRadiusStyleable(): Int = R.styleable.ShapeTextView_shape_bottomRightRadius
+
+    override fun getStartColorStyleable(): Int = R.styleable.ShapeTextView_shape_startColor
+
+    override fun getCenterColorStyleable(): Int = R.styleable.ShapeTextView_shape_centerColor
+
+    override fun getEndColorStyleable(): Int = R.styleable.ShapeTextView_shape_endColor
+
+    override fun getUseLevelStyleable(): Int = R.styleable.ShapeTextView_shape_useLevel
+
+    override fun getAngleStyleable(): Int = R.styleable.ShapeTextView_shape_angle
+
+    override fun getGradientTypeStyleable(): Int = R.styleable.ShapeTextView_shape_gradientType
+
+    override fun getCenterXStyleable(): Int = R.styleable.ShapeTextView_shape_centerX
+
+    override fun getCenterYStyleable(): Int = R.styleable.ShapeTextView_shape_centerY
+
+    override fun getGradientRadiusStyleable(): Int = R.styleable.ShapeTextView_shape_gradientRadius
+
+    override fun getStrokeColorStyleable(): Int = R.styleable.ShapeTextView_shape_strokeColor
+
+    override fun getStrokePressedColorStyleable(): Int = R.styleable.ShapeTextView_shape_strokePressedColor
+
+    override fun getStrokeDisabledColorStyleable(): Int = R.styleable.ShapeTextView_shape_strokeDisabledColor
+
+    override fun getStrokeFocusedColorStyleable(): Int = R.styleable.ShapeTextView_shape_strokeFocusedColor
+
+    override fun getStrokeSelectedColorStyleable(): Int = R.styleable.ShapeTextView_shape_strokeSelectedColor
+
+    override fun getStrokeWidthStyleable(): Int = R.styleable.ShapeTextView_shape_strokeWidth
+
+    override fun getDashWidthStyleable(): Int = R.styleable.ShapeTextView_shape_dashWidth
+
+    override fun getDashGapStyleable(): Int = R.styleable.ShapeTextView_shape_dashGap
+
+    override fun getInnerRadiusStyleable(): Int = R.styleable.ShapeTextView_shape_innerRadius
+
+    override fun getInnerRadiusRatioStyleable(): Int = R.styleable.ShapeTextView_shape_innerRadiusRatio
+
+    override fun getThicknessStyleable(): Int = R.styleable.ShapeTextView_shape_thickness
+
+    override fun getThicknessRatioStyleable(): Int = R.styleable.ShapeTextView_shape_thicknessRatio
+
+    override fun getShadowSizeStyleable(): Int = R.styleable.ShapeTextView_shape_shadowSize
+
+    override fun getShadowColorStyleable(): Int = R.styleable.ShapeTextView_shape_shadowColor
+
+    override fun getShadowOffsetXStyleable(): Int = R.styleable.ShapeTextView_shape_shadowOffsetX
+
+    override fun getShadowOffsetYStyleable(): Int = R.styleable.ShapeTextView_shape_shadowOffsetY
+
+    override fun getTextColorStyleable(): Int = R.styleable.ShapeTextView_shape_textColor
+
+    override fun getTextPressedColorStyleable(): Int = R.styleable.ShapeTextView_shape_textPressedColor
+
+    override fun getTextDisabledColorStyleable(): Int = R.styleable.ShapeTextView_shape_textDisabledColor
+
+    override fun getTextFocusedColorStyleable(): Int = R.styleable.ShapeTextView_shape_textFocusedColor
+
+    override fun getTextSelectedColorStyleable(): Int = R.styleable.ShapeTextView_shape_textSelectedColor
+
+    override fun getTextStartColorStyleable(): Int = R.styleable.ShapeTextView_shape_textStartColor
+
+    override fun getTextCenterColorStyleable(): Int = R.styleable.ShapeTextView_shape_textCenterColor
+
+    override fun getTextEndColorStyleable(): Int = R.styleable.ShapeTextView_shape_textEndColor
+
+    override fun getTextGradientOrientationStyleable(): Int = R.styleable.ShapeTextView_shape_textGradientOrientation
+
+}
