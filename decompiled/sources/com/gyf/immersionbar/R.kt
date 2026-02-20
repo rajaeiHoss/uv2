5a884188507db79f819b61a5714fba8b6880fd60
hossien <h.rajaei@vasl.ir>
2026-02-20 17:11:22 +0330

    Convert com.gyf.immersionbar.R to Kotlin

---
+++ decompiled/sources/com/gyf/immersionbar/R.kt
@@ -0,0 +1,12 @@
+package com.gyf.immersionbar
+
+class R private constructor() {
+    class id private constructor() {
+        companion object {
+            const val action_bar_container: Int = 2131361876
+            const val immersion_fits_layout_overlap: Int = 2131362475
+            const val immersion_navigation_bar_view: Int = 2131362476
+            const val immersion_status_bar_view: Int = 2131362477
+        }
+    }
+}
