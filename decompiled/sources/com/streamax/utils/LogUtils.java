d7993733dff641c439cdc03984ba57354d06b260
hossien <h.rajaei@vasl.ir>
2026-07-10 18:16:04 +0330

    Clarify LogUtils message parameters

--- decompiled/sources/com/streamax/utils/LogUtils.java
+++ decompiled/sources/com/streamax/utils/LogUtils.java
@@ -15,27 +15,27 @@ public class LogUtils {
     private static int mDebuggable = 7;
     private static String mTag = "DeviceConfig";
 
-    public static void log(Class cls, String str) {
+    public static void log(Class cls, String message) {
     }
 
-    public static void log(String str) {
+    public static void log(String message) {
     }
 
-    public static void d(String str) {
+    public static void d(String message) {
         if (mDebuggable >= 2) {
-            Log.d(mTag, str);
+            Log.d(mTag, message);
         }
     }
 
-    public static void i(String str) {
+    public static void i(String message) {
         if (mDebuggable >= 3) {
-            Log.i(mTag, str);
+            Log.i(mTag, message);
         }
     }
 
-    public static void w(String str) {
+    public static void w(String message) {
         if (mDebuggable >= 4) {
-            Log.w(mTag, str);
+            Log.w(mTag, message);
         }
     }
 
@@ -45,28 +45,28 @@ public class LogUtils {
         }
     }
 
-    public static void w(String str, Throwable th) {
-        if (mDebuggable >= 4 && str != null) {
-            Log.w(mTag, str, th);
+    public static void w(String message, Throwable th) {
+        if (mDebuggable >= 4 && message != null) {
+            Log.w(mTag, message, th);
         }
     }
 
-    public static void e(String str) {
+    public static void e(String message) {
         if (mDebuggable >= 5) {
-            Log.e(mTag, str);
+            Log.e(mTag, message);
         }
     }
 
-    public static void sf(String str) {
+    public static void sf(String message) {
         if (mDebuggable >= 5) {
             PrintStream printStream = System.out;
-            printStream.println("----------" + str + "----------");
+            printStream.println("----------" + message + "----------");
         }
     }
 
-    public static void s(String str) {
+    public static void s(String message) {
         if (mDebuggable >= 5) {
-            System.out.println(str);
+            System.out.println(message);
         }
     }
 
@@ -76,33 +76,33 @@ public class LogUtils {
         }
     }
 
-    public static void e(String str, Throwable th) {
-        if (mDebuggable >= 5 && str != null) {
-            Log.e(mTag, str, th);
+    public static void e(String message, Throwable th) {
+        if (mDebuggable >= 5 && message != null) {
+            Log.e(mTag, message, th);
         }
     }
 
-    public static void d(String str, String str2) {
+    public static void d(String tag, String message) {
         if (mDebuggable >= 2) {
-            Log.d(str, str2);
+            Log.d(tag, message);
         }
     }
 
-    public static void i(String str, String str2) {
+    public static void i(String tag, String message) {
         if (mDebuggable >= 3) {
-            Log.i(str, str2);
+            Log.i(tag, message);
         }
     }
 
-    public static void w(String str, String str2) {
+    public static void w(String tag, String message) {
         if (mDebuggable >= 4) {
-            Log.w(str, str2);
+            Log.w(tag, message);
         }
     }
 
-    public static void e(String str, String str2) {
+    public static void e(String tag, String message) {
         if (mDebuggable >= 5) {
-            Log.e(str, str2);
+            Log.e(tag, message);
         }
     }
 }
