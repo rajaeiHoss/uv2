e8df2039ae94478579312e3b7053639f11f89172
hossien <h.rajaei@vasl.ir>
2026-07-10 17:16:03 +0330

    Clarify DevGroupFragment group saving

--- decompiled/sources/com/streamax/client/ui/devlist/ui/DevGroupFragment.java
+++ decompiled/sources/com/streamax/client/ui/devlist/ui/DevGroupFragment.java
@@ -84,27 +84,29 @@ public class DevGroupFragment extends BaseFragment {
         this.mDevDatas = getData();
         if (this.mFlag) {
             this.mGroupDatas = this.mDao.getGroupDatasByName(this.mGroupName);
-            ArrayList arrayList = new ArrayList();
-            for (int i = 0; i < this.mGroupDatas.size(); i++) {
-                if (!arrayList.contains("" + this.mGroupDatas.get(i).dbId)) {
-                    arrayList.add("" + this.mGroupDatas.get(i).dbId);
+            ArrayList<String> groupedDeviceIds = new ArrayList<>();
+            for (int groupIndex = 0; groupIndex < this.mGroupDatas.size(); groupIndex++) {
+                String deviceId = "" + this.mGroupDatas.get(groupIndex).dbId;
+                if (!groupedDeviceIds.contains(deviceId)) {
+                    groupedDeviceIds.add(deviceId);
                 }
             }
-            for (int i2 = 0; i2 < arrayList.size(); i2++) {
+            for (int deviceIndex = 0; deviceIndex < groupedDeviceIds.size(); deviceIndex++) {
                 this.mFlagData01 = new ArrayList<>();
-                for (int i3 = 0; i3 < this.mGroupDatas.size(); i3++) {
-                    if (((String) arrayList.get(i2)).equals("" + this.mGroupDatas.get(i3).dbId) && this.mGroupDatas.get(i3).dbFlag == 1) {
-                        ArrayList<String> arrayList2 = this.mFlagData01;
-                        arrayList2.add("" + this.mGroupDatas.get(i3).dbChannel);
+                String deviceId = groupedDeviceIds.get(deviceIndex);
+                for (int groupIndex = 0; groupIndex < this.mGroupDatas.size(); groupIndex++) {
+                    GroupBean groupBean = this.mGroupDatas.get(groupIndex);
+                    if (deviceId.equals("" + groupBean.dbId) && groupBean.dbFlag == 1) {
+                        this.mFlagData01.add("" + groupBean.dbChannel);
                     }
                 }
                 this.mFlagData02.add(this.mFlagData01);
             }
         } else {
-            for (int i4 = 0; i4 < this.mDevDatas.size(); i4++) {
-                ArrayList<String> arrayList3 = new ArrayList<>();
-                this.mFlagData01 = arrayList3;
-                this.mFlagData02.add(arrayList3);
+            for (int deviceIndex = 0; deviceIndex < this.mDevDatas.size(); deviceIndex++) {
+                ArrayList<String> selectedChannels = new ArrayList<>();
+                this.mFlagData01 = selectedChannels;
+                this.mFlagData02.add(selectedChannels);
             }
         }
         initLayout();
@@ -121,23 +123,23 @@ public class DevGroupFragment extends BaseFragment {
         DevInfoBean devInfoBean;
         this.mEtName.setText(this.mGroupName);
         this.mChildDatas = new ArrayList<>();
-        for (int i = 0; i < this.mDevDatas.size(); i++) {
-            int intValue = ((Integer) this.mDevDatas.get(i).get("id")).intValue();
+        for (int deviceIndex = 0; deviceIndex < this.mDevDatas.size(); deviceIndex++) {
+            int deviceId = getDeviceId(deviceIndex);
             if (MyApp.loginType == 0) {
-                devInfoBean = this.mDbHelper.query(intValue);
+                devInfoBean = this.mDbHelper.query(deviceId);
             } else {
-                devInfoBean = this.mApp.mWebService.query(intValue);
+                devInfoBean = this.mApp.mWebService.query(deviceId);
             }
-            ArrayList arrayList = new ArrayList();
-            int i2 = 0;
-            while (i2 < devInfoBean.mChCounts) {
+            ArrayList<String> channelLabels = new ArrayList<>();
+            int channelNumber = 0;
+            while (channelNumber < devInfoBean.mChCounts) {
                 StringBuilder sb = new StringBuilder();
                 sb.append("通道:");
-                i2++;
-                sb.append(i2);
-                arrayList.add(sb.toString());
+                channelNumber++;
+                sb.append(channelNumber);
+                channelLabels.add(sb.toString());
             }
-            this.mChildDatas.add(arrayList);
+            this.mChildDatas.add(channelLabels);
         }
         this.mAdapter = new CustomExpandAdapter(this.mChildDatas, this.mDevDatas, this.mLv, this.mFlagData02);
         this.mLv.setGroupIndicator((Drawable) null);
@@ -160,144 +162,124 @@ public class DevGroupFragment extends BaseFragment {
         } else {
             list = this.mApp.mWebService.GetTerminalInfoAndroid(true);
         }
-        ArrayList arrayList = new ArrayList();
-        for (int i = 0; i < list.size(); i++) {
-            DevInfoBean devInfoBean = list.get(i);
-            HashMap hashMap = new HashMap();
-            hashMap.put("id", Integer.valueOf(devInfoBean.mDevId));
-            hashMap.put(PlusShare.KEY_CONTENT_DEEP_LINK_METADATA_TITLE, devInfoBean.mDevName);
-            hashMap.put("info", String.format("%s:%s,%s:%s", new Object[]{getString(R.string.deviceip), devInfoBean.mDevIp, getString(R.string.mediaport), Integer.valueOf(devInfoBean.mMediaPort).toString()}));
-            hashMap.put("img", Integer.valueOf(R.drawable.dvronline));
-            arrayList.add(hashMap);
+        ArrayList<Map<String, Object>> devices = new ArrayList<>();
+        for (int deviceIndex = 0; deviceIndex < list.size(); deviceIndex++) {
+            DevInfoBean devInfoBean = list.get(deviceIndex);
+            HashMap<String, Object> deviceRow = new HashMap<>();
+            deviceRow.put("id", Integer.valueOf(devInfoBean.mDevId));
+            deviceRow.put(PlusShare.KEY_CONTENT_DEEP_LINK_METADATA_TITLE, devInfoBean.mDevName);
+            deviceRow.put("info", String.format("%s:%s,%s:%s", new Object[]{getString(R.string.deviceip), devInfoBean.mDevIp, getString(R.string.mediaport), Integer.valueOf(devInfoBean.mMediaPort).toString()}));
+            deviceRow.put("img", Integer.valueOf(R.drawable.dvronline));
+            devices.add(deviceRow);
         }
-        return arrayList;
+        return devices;
     }
 
     public void onClick(View view) {
-        int i;
-        DevInfoBean devInfoBean;
-        DevInfoBean devInfoBean2;
-        int i2;
-        DevInfoBean devInfoBean3;
-        int i3;
-        DevInfoBean devInfoBean4;
-        int i4;
         switch (view.getId()) {
             case R.id.dev_title_btn_left /*2131362303*/:
                 FragmentUtils.fragmentReplace(this, new DevListFragment(), R.id.dev_fl);
                 return;
             case R.id.dev_title_btn_right /*2131362304*/:
-                String trim = this.mEtName.getText().toString().trim();
-                if (TextUtils.isEmpty(trim)) {
+                String newGroupName = this.mEtName.getText().toString().trim();
+                if (TextUtils.isEmpty(newGroupName)) {
                     ToastSf.toastSf((Activity) this.mActivity, (int) R.string.group_GroupIsEmpty);
                     return;
                 }
-                if (MyApp.loginType == 0) {
-                    i = 0;
-                    for (int i5 = 0; i5 < this.mDevDatas.size(); i5++) {
-                        DevInfoBean query = this.mDbHelper.query(((Integer) this.mDevDatas.get(i5).get("id")).intValue());
-                        for (int i6 = 0; i6 < query.mChCounts; i6++) {
-                            if (this.mFlagData02.get(i5).contains("" + i6)) {
-                                i++;
-                            }
-                        }
-                    }
-                } else {
-                    i = 0;
-                    for (int i7 = 0; i7 < this.mDevDatas.size(); i7++) {
-                        DevInfoBean query2 = this.mApp.mWebService.query(((Integer) this.mDevDatas.get(i7).get("id")).intValue());
-                        for (int i8 = 0; i8 < query2.mChCounts; i8++) {
-                            if (this.mFlagData02.get(i7).contains("" + i8)) {
-                                i++;
-                            }
-                        }
-                    }
-                }
-                if (i > 32) {
+                if (countSelectedChannels() > 32) {
                     ToastSf.toastSf((Activity) this.mActivity, "selected channel more than 32");
                     return;
                 }
                 if (this.mFlag) {
-                    if (MyApp.loginType == 0) {
-                        for (int i9 = 0; i9 < this.mDevDatas.size(); i9++) {
-                            int intValue = ((Integer) this.mDevDatas.get(i9).get("id")).intValue();
-                            String str = (String) this.mDevDatas.get(i9).get(PlusShare.KEY_CONTENT_DEEP_LINK_METADATA_TITLE);
-                            DevInfoBean query3 = this.mDbHelper.query(intValue);
-                            int i10 = 0;
-                            while (i10 < query3.mChCounts) {
-                                if (this.mFlagData02.get(i9).contains("" + i10)) {
-                                    i4 = i10;
-                                    devInfoBean4 = query3;
-                                    this.mDao.update(trim, intValue, str, i10, 1);
-                                } else {
-                                    i4 = i10;
-                                    devInfoBean4 = query3;
-                                    this.mDao.update(trim, intValue, str, i10, 0);
-                                }
-                                i10 = i4 + 1;
-                                query3 = devInfoBean4;
-                            }
-                        }
-                    } else {
-                        for (int i11 = 0; i11 < this.mDevDatas.size(); i11++) {
-                            int intValue2 = ((Integer) this.mDevDatas.get(i11).get("id")).intValue();
-                            String str2 = (String) this.mDevDatas.get(i11).get(PlusShare.KEY_CONTENT_DEEP_LINK_METADATA_TITLE);
-                            DevInfoBean query4 = this.mApp.mWebService.query(intValue2);
-                            int i12 = 0;
-                            while (i12 < query4.mChCounts) {
-                                if (this.mFlagData02.get(i11).contains("" + i12)) {
-                                    i3 = i12;
-                                    devInfoBean3 = query4;
-                                    this.mDao.add(trim, intValue2, str2, i12, 1);
-                                } else {
-                                    i3 = i12;
-                                    devInfoBean3 = query4;
-                                    this.mDao.add(trim, intValue2, str2, i12, 0);
-                                }
-                                i12 = i3 + 1;
-                                query4 = devInfoBean3;
-                            }
-                        }
-                    }
-                    if (!this.mGroupName.equals(this.mEtName.getText().toString().trim())) {
-                        for (int i13 = 0; i13 < this.mGroupDatas.size(); i13++) {
-                            this.mDao.update(this.mEtName.getText().toString().trim(), this.mGroupName);
-                        }
-                    }
-                } else if (!this.mDao.query(trim)) {
+                    saveEditedGroup(newGroupName);
+                } else if (!this.mDao.query(newGroupName)) {
                     ToastSf.toastSf((Activity) this.mActivity, (int) R.string.group_GroupIsExist);
                     return;
                 } else {
-                    for (int i14 = 0; i14 < this.mDevDatas.size(); i14++) {
-                        int intValue3 = ((Integer) this.mDevDatas.get(i14).get("id")).intValue();
-                        String str3 = (String) this.mDevDatas.get(i14).get(PlusShare.KEY_CONTENT_DEEP_LINK_METADATA_TITLE);
-                        if (MyApp.loginType == 0) {
-                            devInfoBean = this.mDbHelper.query(intValue3);
-                        } else {
-                            devInfoBean = this.mApp.mWebService.query(intValue3);
-                        }
-                        DevInfoBean devInfoBean5 = devInfoBean;
-                        int i15 = 0;
-                        while (i15 < devInfoBean5.mChCounts) {
-                            if (this.mFlagData02.get(i14).contains("" + i15)) {
-                                i2 = i15;
-                                devInfoBean2 = devInfoBean5;
-                                this.mDao.add(trim, intValue3, str3, i15, 1);
-                            } else {
-                                i2 = i15;
-                                devInfoBean2 = devInfoBean5;
-                                this.mDao.add(trim, intValue3, str3, i15, 0);
-                            }
-                            i15 = i2 + 1;
-                            devInfoBean5 = devInfoBean2;
-                        }
-                    }
+                    saveNewGroup(newGroupName);
                 }
                 FragmentUtils.fragmentReplace(this, new DevListFragment(), R.id.dev_fl);
                 return;
             default:
                 return;
         }
+    }
+
+    private void saveEditedGroup(String newGroupName) {
+        if (MyApp.loginType == 0) {
+            for (int deviceIndex = 0; deviceIndex < this.mDevDatas.size(); deviceIndex++) {
+                int deviceId = getDeviceId(deviceIndex);
+                String deviceName = getDeviceName(deviceIndex);
+                DevInfoBean deviceInfo = this.mDbHelper.query(deviceId);
+                for (int channelIndex = 0; channelIndex < deviceInfo.mChCounts; channelIndex++) {
+                    this.mDao.update(newGroupName, deviceId, deviceName, channelIndex, getChannelFlag(deviceIndex, channelIndex));
+                }
+            }
+        } else {
+            for (int deviceIndex = 0; deviceIndex < this.mDevDatas.size(); deviceIndex++) {
+                int deviceId = getDeviceId(deviceIndex);
+                String deviceName = getDeviceName(deviceIndex);
+                DevInfoBean deviceInfo = this.mApp.mWebService.query(deviceId);
+                for (int channelIndex = 0; channelIndex < deviceInfo.mChCounts; channelIndex++) {
+                    this.mDao.add(newGroupName, deviceId, deviceName, channelIndex, getChannelFlag(deviceIndex, channelIndex));
+                }
+            }
+        }
+        if (!this.mGroupName.equals(newGroupName)) {
+            for (int groupIndex = 0; groupIndex < this.mGroupDatas.size(); groupIndex++) {
+                this.mDao.update(newGroupName, this.mGroupName);
+            }
+        }
+    }
+
+    private void saveNewGroup(String newGroupName) {
+        for (int deviceIndex = 0; deviceIndex < this.mDevDatas.size(); deviceIndex++) {
+            int deviceId = getDeviceId(deviceIndex);
+            String deviceName = getDeviceName(deviceIndex);
+            DevInfoBean deviceInfo;
+            if (MyApp.loginType == 0) {
+                deviceInfo = this.mDbHelper.query(deviceId);
+            } else {
+                deviceInfo = this.mApp.mWebService.query(deviceId);
+            }
+            for (int channelIndex = 0; channelIndex < deviceInfo.mChCounts; channelIndex++) {
+                this.mDao.add(newGroupName, deviceId, deviceName, channelIndex, getChannelFlag(deviceIndex, channelIndex));
+            }
+        }
+    }
+
+    private int countSelectedChannels() {
+        int selectedCount = 0;
+        for (int deviceIndex = 0; deviceIndex < this.mDevDatas.size(); deviceIndex++) {
+            DevInfoBean deviceInfo;
+            if (MyApp.loginType == 0) {
+                deviceInfo = this.mDbHelper.query(getDeviceId(deviceIndex));
+            } else {
+                deviceInfo = this.mApp.mWebService.query(getDeviceId(deviceIndex));
+            }
+            for (int channelIndex = 0; channelIndex < deviceInfo.mChCounts; channelIndex++) {
+                if (isChannelSelected(deviceIndex, channelIndex)) {
+                    selectedCount++;
+                }
+            }
+        }
+        return selectedCount;
+    }
+
+    private int getChannelFlag(int deviceIndex, int channelIndex) {
+        return isChannelSelected(deviceIndex, channelIndex) ? 1 : 0;
+    }
+
+    private boolean isChannelSelected(int deviceIndex, int channelIndex) {
+        return this.mFlagData02.get(deviceIndex).contains("" + channelIndex);
+    }
+
+    private int getDeviceId(int deviceIndex) {
+        return ((Integer) this.mDevDatas.get(deviceIndex).get("id")).intValue();
+    }
+
+    private String getDeviceName(int deviceIndex) {
+        return (String) this.mDevDatas.get(deviceIndex).get(PlusShare.KEY_CONTENT_DEEP_LINK_METADATA_TITLE);
     }
 
     public void onDestroy() {
