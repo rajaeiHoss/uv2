d5874e4d4574a50f18a6275cbe797c4113cd9b7c
hossien <h.rajaei@vasl.ir>
2026-07-10 15:46:50 +0330

    Reconstruct CustomImageView frame input

--- decompiled/sources/com/streamax/client/CustomImageView.java
+++ decompiled/sources/com/streamax/client/CustomImageView.java
@@ -39,9 +39,9 @@ public class CustomImageView extends ImageView {
         }
     }
 
-    public CustomImageView(Context context, int i) {
+    public CustomImageView(Context context, int serial) {
         super(context);
-        this.mSerial = i;
+        this.mSerial = serial;
         Paint paint = new Paint();
         this.mPaint = paint;
         paint.setAntiAlias(true);
@@ -63,144 +63,52 @@ public class CustomImageView extends ImageView {
         this.mOneFrame = new OneFrame();
     }
 
-    public void setBkColor(int i) {
-        this.mBkColor = i;
+    public void setBkColor(int color) {
+        this.mBkColor = color;
     }
 
-    public void setFocusState(boolean z) {
-        this.mbFocus = z;
+    public void setFocusState(boolean focused) {
+        this.mbFocus = focused;
         postInvalidate();
     }
 
-    public void setDragState(boolean z) {
-        this.mbDrag = z;
+    public void setDragState(boolean dragging) {
+        this.mbDrag = dragging;
         postInvalidate();
     }
 
-    public void setOsdState(boolean z) {
-        this.mbOsd = z;
+    public void setOsdState(boolean enabled) {
+        this.mbOsd = enabled;
         postInvalidate();
     }
 
-    public void setMax(boolean z) {
-        this.mbMax = z;
+    public void setMax(boolean maximized) {
+        this.mbMax = maximized;
         postInvalidate();
     }
 
-    /* JADX WARNING: Can't wrap try/catch for region: R(7:16|17|18|19|20|21|22) */
-    /* JADX WARNING: Missing exception handler attribute for start block: B:18:0x0038 */
-    /* Code decompiled incorrectly, please refer to instructions dump. */
-    public void writeIn(byte[] r5, int r6, int r7) {
-        /*
-            r4 = this;
-            if (r5 == 0) goto L_0x0048
-            if (r6 <= 0) goto L_0x0048
-            if (r7 > 0) goto L_0x0007
-            goto L_0x0048
-        L_0x0007:
-            int r0 = r6 * r7
-            int r0 = r0 * 2
-            com.streamax.client.CustomImageView$OneFrame r1 = r4.mOneFrame
-            monitor-enter(r1)
-            com.streamax.client.CustomImageView$OneFrame r2 = r4.mOneFrame     // Catch:{ all -> 0x0045 }
-            byte[] r2 = r2.data     // Catch:{ all -> 0x0045 }
-            if (r2 != 0) goto L_0x001b
-            com.streamax.client.CustomImageView$OneFrame r2 = r4.mOneFrame     // Catch:{ all -> 0x0045 }
-            byte[] r3 = new byte[r0]     // Catch:{ all -> 0x0045 }
-            r2.data = r3     // Catch:{ all -> 0x0045 }
-            goto L_0x0028
-        L_0x001b:
-            com.streamax.client.CustomImageView$OneFrame r2 = r4.mOneFrame     // Catch:{ all -> 0x0045 }
-            byte[] r2 = r2.data     // Catch:{ all -> 0x0045 }
-            int r2 = r2.length     // Catch:{ all -> 0x0045 }
-            if (r2 >= r0) goto L_0x0028
-            com.streamax.client.CustomImageView$OneFrame r2 = r4.mOneFrame     // Catch:{ all -> 0x0045 }
-            byte[] r3 = new byte[r0]     // Catch:{ all -> 0x0045 }
-            r2.data = r3     // Catch:{ all -> 0x0045 }
-        L_0x0028:
-            com.streamax.client.CustomImageView$OneFrame r2 = r4.mOneFrame     // Catch:{ all -> 0x0045 }
-            byte[] r2 = r2.data     // Catch:{ all -> 0x0045 }
-            if (r2 != 0) goto L_0x0030
-            monitor-exit(r1)     // Catch:{ all -> 0x0045 }
-            return
-        L_0x0030:
-            com.streamax.client.CustomImageView$OneFrame r2 = r4.mOneFrame     // Catch:{ IndexOutOfBoundsException -> 0x0038 }
-            byte[] r2 = r2.data     // Catch:{ IndexOutOfBoundsException -> 0x0038 }
-            r3 = 0
-            java.lang.System.arraycopy(r5, r3, r2, r3, r0)     // Catch:{ IndexOutOfBoundsException -> 0x0038 }
-        L_0x0038:
-            com.streamax.client.CustomImageView$OneFrame r5 = r4.mOneFrame     // Catch:{ all -> 0x0045 }
-            r5.width = r6     // Catch:{ all -> 0x0045 }
-            com.streamax.client.CustomImageView$OneFrame r5 = r4.mOneFrame     // Catch:{ all -> 0x0045 }
-            r5.height = r7     // Catch:{ all -> 0x0045 }
-            monitor-exit(r1)     // Catch:{ all -> 0x0045 }
-            r4.postInvalidate()
-            return
-        L_0x0045:
-            r5 = move-exception
-            monitor-exit(r1)     // Catch:{ all -> 0x0045 }
-            throw r5
-        L_0x0048:
-            return
-        */
-        throw new UnsupportedOperationException("Method not decompiled: com.streamax.client.CustomImageView.writeIn(byte[], int, int):void");
+    public void writeIn(byte[] data, int width, int height) {
+        if (data == null || width <= 0 || height <= 0) {
+            return;
+        }
+        int frameSize = width * height * 2;
+        synchronized (this.mOneFrame) {
+            if (this.mOneFrame.data == null || this.mOneFrame.data.length < frameSize) {
+                this.mOneFrame.data = new byte[frameSize];
+            }
+            if (this.mOneFrame.data == null) {
+                return;
+            }
+            try {
+                System.arraycopy(data, 0, this.mOneFrame.data, 0, frameSize);
+            } catch (IndexOutOfBoundsException unused) {
+            }
+            this.mOneFrame.width = width;
+            this.mOneFrame.height = height;
+        }
+        postInvalidate();
     }
 
-    /* JADX WARNING: Code restructure failed: missing block: B:30:0x0075, code lost:
-        if (r9.mMatrix != null) goto L_0x007e;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:31:0x0077, code lost:
-        r9.mMatrix = new android.graphics.Matrix();
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:33:0x0080, code lost:
-        if (r9.mPoint != null) goto L_0x0089;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:34:0x0082, code lost:
-        r9.mPoint = new android.graphics.PointF();
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:36:0x008e, code lost:
-        if (r9.mScales < 0.0f) goto L_0x0104;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:37:0x0090, code lost:
-        r0 = ((float) (getWidth() - 2)) / ((float) r9.mWidth);
-        r1 = ((float) (getHeight() - 2)) / ((float) r9.mHeight);
-        r2 = r9.mScales;
-        r3 = r0 * r2;
-        r2 = r2 * r1;
-        r4 = r9.mTranslate.x / r3;
-        r5 = r9.mTranslate.y / r2;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:38:0x00ba, code lost:
-        if (r3 <= r0) goto L_0x00db;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:39:0x00bc, code lost:
-        r9.mPoint.x = (((r3 - r0) * ((float) r9.mWidth)) / (r3 * 2.0f)) + r4;
-        r9.mPoint.y = (((r2 - r1) * ((float) r9.mHeight)) / (2.0f * r2)) + r5;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:40:0x00db, code lost:
-        r9.mPoint.x = (((r0 - r3) * ((float) r9.mWidth)) / (r3 * 2.0f)) + r4;
-        r9.mPoint.y = (((r1 - r2) * ((float) r9.mHeight)) / (2.0f * r2)) + r5;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:41:0x00f7, code lost:
-        r9.mMatrix.setScale(r3, r2, r9.mPoint.x, r9.mPoint.y);
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:42:0x0104, code lost:
-        r10.drawColor(r9.mBkColor);
-        r0 = r9.mVideoBit;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:43:0x010b, code lost:
-        if (r0 == null) goto L_0x0113;
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:44:0x010d, code lost:
-        r10.drawBitmap(r0, r9.mMatrix, (android.graphics.Paint) null);
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:45:0x0113, code lost:
-        doPaint(r10);
-     */
-    /* JADX WARNING: Code restructure failed: missing block: B:46:0x0116, code lost:
-        return;
-     */
-    /* Code decompiled incorrectly, please refer to instructions dump. */
     public void deDraw(android.graphics.Canvas canvas) {
         synchronized (this.mOneFrame) {
             if (this.mOneFrame.data == null || this.mOneFrame.data.length <= 0 || this.mOneFrame.width == 0 || this.mOneFrame.height == 0) {
@@ -254,10 +162,10 @@ public class CustomImageView extends ImageView {
         doPaint(canvas);
     }
 
-    public void setDigitalZoomIn(float f) {
-        float f2 = this.mScales;
-        if (f2 * f > 0.0f) {
-            this.mScales = f2 * f;
+    public void setDigitalZoomIn(float scaleFactor) {
+        float previousScale = this.mScales;
+        if (previousScale * scaleFactor > 0.0f) {
+            this.mScales = previousScale * scaleFactor;
             this.mTranslate.set(0.0f, 0.0f);
         }
         invalidate();
@@ -273,11 +181,11 @@ public class CustomImageView extends ImageView {
         invalidate();
     }
 
-    public void setDigitalTranslate(float f, float f2) {
+    public void setDigitalTranslate(float deltaX, float deltaY) {
         if (this.mTranslate == null) {
             this.mTranslate = new PointF();
         }
-        this.mTranslate.offset(f, f2);
+        this.mTranslate.offset(deltaX, deltaY);
         invalidate();
     }
 
