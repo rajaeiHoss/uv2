27ec73191968f4e2f51d009226f58b16ba8ab30a
hossien <h.rajaei@vasl.ir>
2026-07-13 14:40:34 +0330

    Clarify TimeUtils time variables

--- decompiled/sources/com/streamax/utils/TimeUtils.java
+++ decompiled/sources/com/streamax/utils/TimeUtils.java
@@ -22,28 +22,28 @@ public class TimeUtils {
         type = type2;
     }
 
-    public static String getTime(Date date, String str) {
-        return new SimpleDateFormat(str).format(date);
+    public static String getTime(Date date, String pattern) {
+        return new SimpleDateFormat(pattern).format(date);
     }
 
-    public static String parse2String_86399(int i) {
-        if (i <= 0 || i > 86400) {
+    public static String parse2String_86399(int secondsOfDay) {
+        if (secondsOfDay <= 0 || secondsOfDay > 86400) {
             return "00:00:00";
         }
-        int i2 = i / 3600;
-        int i3 = i - (i2 * 3600);
-        return parse2Str_86399(i2, i3 / 60, i3 % 60);
+        int hours = secondsOfDay / 3600;
+        int remainingSeconds = secondsOfDay - (hours * 3600);
+        return parse2Str_86399(hours, remainingSeconds / 60, remainingSeconds % 60);
     }
 
-    public static String parse2Str_86399(int i, int i2, int i3) {
-        return String.format("%02d:%02d:%02d", new Object[]{Integer.valueOf(i), Integer.valueOf(i2), Integer.valueOf(i3)});
+    public static String parse2Str_86399(int hours, int minutes, int seconds) {
+        return String.format("%02d:%02d:%02d", new Object[]{Integer.valueOf(hours), Integer.valueOf(minutes), Integer.valueOf(seconds)});
     }
 
-    public static int parse2Int_86399(String str) {
-        if (str.length() != 8) {
+    public static int parse2Int_86399(String timeText) {
+        if (timeText.length() != 8) {
             return 0;
         }
-        return (StringUtils.parse2Int(str.substring(0, 2)) * 3600) + (StringUtils.parse2Int(str.substring(3, 5)) * 60) + StringUtils.parse2Int(str.substring(6, 8));
+        return (StringUtils.parse2Int(timeText.substring(0, 2)) * 3600) + (StringUtils.parse2Int(timeText.substring(3, 5)) * 60) + StringUtils.parse2Int(timeText.substring(6, 8));
     }
 
     public static int parse2Int_86399(TextView textView) {
@@ -67,13 +67,13 @@ public class TimeUtils {
         });
     }
 
-    public static void setTimePickListener(Activity activity, TextView textView, TimePickerView.Type type2, TimePickListener timePickListener) {
-        type = type2;
+    public static void setTimePickListener(Activity activity, TextView textView, TimePickerView.Type pickerType, TimePickListener timePickListener) {
+        type = pickerType;
         setTimePickListener(activity, textView, timePickListener);
     }
 
-    public static void setTimePickListener(Activity activity, final TextView textView, Date date, TimePickerView.Type type2, final TimePickListener timePickListener) {
-        TimePickerView timePickerView = new TimePickerView(activity, type2);
+    public static void setTimePickListener(Activity activity, final TextView textView, Date date, TimePickerView.Type pickerType, final TimePickListener timePickListener) {
+        TimePickerView timePickerView = new TimePickerView(activity, pickerType);
         timePickerView.setTime(date);
         timePickerView.setCyclic(false);
         timePickerView.setCancelable(true);
