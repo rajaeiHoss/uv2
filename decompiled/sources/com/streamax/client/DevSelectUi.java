13ede34e97b692088690ebac51db39b47a3994b1
hossien <h.rajaei@vasl.ir>
2026-07-13 14:12:02 +0330

    Clarify DevSelectUi channel selection variables

--- decompiled/sources/com/streamax/client/DevSelectUi.java
+++ decompiled/sources/com/streamax/client/DevSelectUi.java
@@ -47,7 +47,7 @@ public class DevSelectUi extends BaseUi implements ExpandableListView.OnGroupCli
     private TextView mTvTitle;
     private DbHelper mdbHelper;
 
-    public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long j) {
+    public boolean onGroupClick(ExpandableListView expandableListView, View view, int groupPosition, long id) {
         return false;
     }
 
@@ -76,20 +76,20 @@ public class DevSelectUi extends BaseUi implements ExpandableListView.OnGroupCli
         this.mGroupNameDatas = new ArrayList();
         List<GroupBean> list = this.mGroupBeanDatas;
         if (list != null && list.size() > 0) {
-            for (int i = 0; i < this.mGroupBeanDatas.size(); i++) {
-                if (!this.mGroupNameDatas.contains(this.mGroupBeanDatas.get(i).dbGroupName)) {
-                    this.mGroupNameDatas.add(this.mGroupBeanDatas.get(i).dbGroupName);
+            for (int groupBeanIndex = 0; groupBeanIndex < this.mGroupBeanDatas.size(); groupBeanIndex++) {
+                if (!this.mGroupNameDatas.contains(this.mGroupBeanDatas.get(groupBeanIndex).dbGroupName)) {
+                    this.mGroupNameDatas.add(this.mGroupBeanDatas.get(groupBeanIndex).dbGroupName);
                 }
             }
             this.mDatas = new ArrayList<>();
-            for (int i2 = 0; i2 < this.mGroupNameDatas.size(); i2++) {
-                ArrayList arrayList = new ArrayList();
-                for (int i3 = 0; i3 < this.mGroupBeanDatas.size(); i3++) {
-                    if (this.mGroupNameDatas.get(i2).equals(this.mGroupBeanDatas.get(i3).dbGroupName) && this.mGroupBeanDatas.get(i3).dbFlag == 1) {
-                        arrayList.add(this.mGroupBeanDatas.get(i3));
+            for (int groupNameIndex = 0; groupNameIndex < this.mGroupNameDatas.size(); groupNameIndex++) {
+                ArrayList<GroupBean> enabledGroupChannels = new ArrayList<>();
+                for (int groupBeanIndex = 0; groupBeanIndex < this.mGroupBeanDatas.size(); groupBeanIndex++) {
+                    if (this.mGroupNameDatas.get(groupNameIndex).equals(this.mGroupBeanDatas.get(groupBeanIndex).dbGroupName) && this.mGroupBeanDatas.get(groupBeanIndex).dbFlag == 1) {
+                        enabledGroupChannels.add(this.mGroupBeanDatas.get(groupBeanIndex));
                     }
                 }
-                this.mDatas.add(arrayList);
+                this.mDatas.add(enabledGroupChannels);
             }
         }
         this.mLv.setAdapter(new CustomAdapter());
@@ -106,19 +106,19 @@ public class DevSelectUi extends BaseUi implements ExpandableListView.OnGroupCli
         }
         List<DevInfoBean> list = this.mDevInfoDatas;
         if (list != null && list.size() > 0) {
-            for (int i = 0; i < this.mDevInfoDatas.size(); i++) {
-                this.mGroupDatas.add(this.mDevInfoDatas.get(i).mDevName);
-                ArrayList arrayList = new ArrayList();
-                int i2 = 0;
-                while (i2 < this.mDevInfoDatas.get(i).mChCounts) {
-                    StringBuilder sb = new StringBuilder();
-                    sb.append(this.mResources.getString(R.string.channel));
-                    sb.append(" ");
-                    i2++;
-                    sb.append(i2);
-                    arrayList.add(sb.toString());
+            for (int deviceIndex = 0; deviceIndex < this.mDevInfoDatas.size(); deviceIndex++) {
+                this.mGroupDatas.add(this.mDevInfoDatas.get(deviceIndex).mDevName);
+                ArrayList<String> channelNames = new ArrayList<>();
+                int channelIndex = 0;
+                while (channelIndex < this.mDevInfoDatas.get(deviceIndex).mChCounts) {
+                    StringBuilder channelLabel = new StringBuilder();
+                    channelLabel.append(this.mResources.getString(R.string.channel));
+                    channelLabel.append(" ");
+                    channelIndex++;
+                    channelLabel.append(channelIndex);
+                    channelNames.add(channelLabel.toString());
                 }
-                this.mChildDatas.add(arrayList);
+                this.mChildDatas.add(channelNames);
             }
         }
     }
@@ -138,19 +138,19 @@ public class DevSelectUi extends BaseUi implements ExpandableListView.OnGroupCli
     }
 
     class CustomAdapter extends BaseExpandableListAdapter {
-        public Object getChild(int i, int i2) {
+        public Object getChild(int groupPosition, int childPosition) {
             return "childPosition";
         }
 
-        public long getChildId(int i, int i2) {
+        public long getChildId(int groupPosition, int childPosition) {
             return 0;
         }
 
-        public Object getGroup(int i) {
+        public Object getGroup(int groupPosition) {
             return "groupPosition";
         }
 
-        public long getGroupId(int i) {
+        public long getGroupId(int groupPosition) {
             return 0;
         }
 
@@ -158,7 +158,7 @@ public class DevSelectUi extends BaseUi implements ExpandableListView.OnGroupCli
             return false;
         }
 
-        public boolean isChildSelectable(int i, int i2) {
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
             return false;
         }
 
@@ -166,68 +166,72 @@ public class DevSelectUi extends BaseUi implements ExpandableListView.OnGroupCli
         }
 
         public int getGroupCount() {
-            int i = 0;
+            int groupCount = 0;
             if (DevSelectUi.this.mGroupDatas != null) {
-                i = 0 + DevSelectUi.this.mGroupDatas.size();
+                groupCount = DevSelectUi.this.mGroupDatas.size();
             }
-            return (DevSelectUi.this.mGroupBeanDatas == null || DevSelectUi.this.mDatas == null) ? i : i + DevSelectUi.this.mDatas.size();
+            return (DevSelectUi.this.mGroupBeanDatas == null || DevSelectUi.this.mDatas == null) ? groupCount : groupCount + DevSelectUi.this.mDatas.size();
         }
 
         public int getGroupTypeCount() {
             return DevSelectUi.this.mGroupBeanDatas == null ? 1 : 2;
         }
 
-        public int getChildrenCount(int i) {
+        public int getChildrenCount(int groupPosition) {
             if (DevSelectUi.this.mChildDatas == null) {
                 return 0;
             }
-            if (i < DevSelectUi.this.mChildDatas.size()) {
-                return DevSelectUi.this.mChildDatas.get(i).size();
+            if (groupPosition < DevSelectUi.this.mChildDatas.size()) {
+                return DevSelectUi.this.mChildDatas.get(groupPosition).size();
             }
-            return ((ArrayList) DevSelectUi.this.mDatas.get(i - DevSelectUi.this.mChildDatas.size())).size();
+            return ((ArrayList) DevSelectUi.this.mDatas.get(groupPosition - DevSelectUi.this.mChildDatas.size())).size();
         }
 
-        public View getGroupView(int i, boolean z, View view, ViewGroup viewGroup) {
+        public View getGroupView(int groupPosition, boolean isExpanded, View view, ViewGroup viewGroup) {
             if (view == null) {
                 view = View.inflate(DevSelectUi.this.mContext, R.layout.exp_group, (ViewGroup) null);
                 new GroupHolder(view);
             }
             GroupHolder groupHolder = (GroupHolder) view.getTag();
-            groupHolder.mGroupIv.setImageResource(z ? R.drawable.arrow_down : R.drawable.arrow_right);
+            groupHolder.mGroupIv.setImageResource(isExpanded ? R.drawable.arrow_down : R.drawable.arrow_right);
             if (DevSelectUi.this.mGroupDatas != null && DevSelectUi.this.mGroupDatas.size() > 0) {
-                if (i < DevSelectUi.this.mGroupDatas.size()) {
-                    groupHolder.mGroupTv.setText(DevSelectUi.this.mGroupDatas.get(i));
+                if (groupPosition < DevSelectUi.this.mGroupDatas.size()) {
+                    groupHolder.mGroupTv.setText(DevSelectUi.this.mGroupDatas.get(groupPosition));
                 } else {
-                    groupHolder.mGroupTv.setText((CharSequence) DevSelectUi.this.mGroupNameDatas.get(i - DevSelectUi.this.mGroupDatas.size()));
+                    groupHolder.mGroupTv.setText((CharSequence) DevSelectUi.this.mGroupNameDatas.get(groupPosition - DevSelectUi.this.mGroupDatas.size()));
                 }
             }
             return view;
         }
 
-        public View getChildView(final int i, final int i2, boolean z, View view, ViewGroup viewGroup) {
+        public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View view, ViewGroup viewGroup) {
             if (view == null) {
                 view = View.inflate(DevSelectUi.this.mContext, R.layout.exp_child, (ViewGroup) null);
                 new ChildHolder(view);
             }
             ChildHolder childHolder = (ChildHolder) view.getTag();
             if (DevSelectUi.this.mGroupDatas != null && DevSelectUi.this.mGroupDatas.size() > 0) {
-                if (i < DevSelectUi.this.mGroupDatas.size()) {
-                    childHolder.mChildTv.setText((CharSequence) DevSelectUi.this.mChildDatas.get(i).get(i2));
+                if (groupPosition < DevSelectUi.this.mGroupDatas.size()) {
+                    childHolder.mChildTv.setText((CharSequence) DevSelectUi.this.mChildDatas.get(groupPosition).get(childPosition));
                 } else {
+                    int groupOffset = groupPosition - DevSelectUi.this.mGroupDatas.size();
+                    GroupBean groupChannel = (GroupBean) ((ArrayList) DevSelectUi.this.mDatas.get(groupOffset)).get(childPosition);
                     TextView textView = childHolder.mChildTv;
-                    textView.setText(DevSelectUi.this.mResources.getString(R.string.channel) + " " + (((GroupBean) ((ArrayList) DevSelectUi.this.mDatas.get(i - DevSelectUi.this.mGroupDatas.size())).get(i2)).dbChannel + 1));
+                    textView.setText(DevSelectUi.this.mResources.getString(R.string.channel) + " " + (groupChannel.dbChannel + 1));
                 }
             }
             childHolder.mChildLl.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
                     Intent intent = new Intent(DevSelectUi.this, RealPlayActivity.class);
-                    if (i < DevSelectUi.this.mGroupDatas.size()) {
-                        intent.putExtra("id", ((DevInfoBean) DevSelectUi.this.mDevInfoDatas.get(i)).mDevId);
-                        intent.putExtra("channel", i2);
+                    if (groupPosition < DevSelectUi.this.mGroupDatas.size()) {
+                        intent.putExtra("id", ((DevInfoBean) DevSelectUi.this.mDevInfoDatas.get(groupPosition)).mDevId);
+                        intent.putExtra("channel", childPosition);
                         intent.putExtra("currentPosition", DevSelectUi.this.currentPosition);
                     } else {
-                        intent.putExtra("id", ((GroupBean) ((ArrayList) DevSelectUi.this.mDatas.get(i - DevSelectUi.this.mGroupDatas.size())).get(i2)).dbId);
-                        intent.putExtra("channel", ((GroupBean) ((ArrayList) DevSelectUi.this.mDatas.get(i - DevSelectUi.this.mGroupDatas.size())).get(i2)).dbChannel);
+                        int groupOffset = groupPosition - DevSelectUi.this.mGroupDatas.size();
+                        GroupBean groupChannel = (GroupBean) ((ArrayList) DevSelectUi.this.mDatas.get(groupOffset)).get(childPosition);
+                        intent.putExtra("id", groupChannel.dbId);
+                        intent.putExtra("channel", groupChannel.dbChannel);
                         intent.putExtra("currentPosition", DevSelectUi.this.currentPosition);
                     }
                     DevSelectUi.this.setResult(-1, intent);
