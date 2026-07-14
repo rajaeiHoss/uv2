79ccbc68ed11d9df41b96649f7daaf2d40aaf579
hossien <h.rajaei@vasl.ir>
2026-07-14 11:09:21 +0330

    Clarify PlayFragmentFile adapter variables

--- decompiled/sources/com/streamax/client/PlayFragmentFile.java
+++ decompiled/sources/com/streamax/client/PlayFragmentFile.java
@@ -36,9 +36,9 @@ public class PlayFragmentFile extends FragmentBase implements View.OnClickListen
         }
     }
 
-    public static PlayFragmentFile getInstance(String str, PlayActivity playActivity) {
+    public static PlayFragmentFile getInstance(String title, PlayActivity playActivity) {
         PlayFragmentFile playFragmentFile = new PlayFragmentFile();
-        playFragmentFile.mTitle = str;
+        playFragmentFile.mTitle = title;
         playFragmentFile.mActivity = playActivity;
         return playFragmentFile;
     }
@@ -57,31 +57,31 @@ public class PlayFragmentFile extends FragmentBase implements View.OnClickListen
     /* access modifiers changed from: protected */
     public void initEvent() {
         this.mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long j) {
-                PlayFragmentFile.this.onFileItemClick(i);
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                PlayFragmentFile.this.onFileItemClick(position);
             }
         });
         this.mBtnBack.setOnClickListener(this);
     }
 
     /* access modifiers changed from: private */
-    public void onFileItemClick(int i) {
-        if (i <= this.mFileList.size()) {
-            for (int i2 = 0; i2 < this.mFileList.size(); i2++) {
-                this.mFileList.get(i2).mIsPlay = false;
+    public void onFileItemClick(int position) {
+        if (position < this.mFileList.size()) {
+            for (int fileIndex = 0; fileIndex < this.mFileList.size(); fileIndex++) {
+                this.mFileList.get(fileIndex).mIsPlay = false;
             }
-            this.mFileList.get(i).mIsPlay = true;
+            this.mFileList.get(position).mIsPlay = true;
             this.mFileAdapter.notifyDataSetChanged();
-            this.mActivity.playNewFile(this.mFileList.get(i));
+            this.mActivity.playNewFile(this.mFileList.get(position));
         }
     }
 
     /* access modifiers changed from: private */
-    public void onFileDownClick(int i) {
+    public void onFileDownClick(int position) {
         if (Build.VERSION.SDK_INT >= 30 && !Environment.isExternalStorageManager()) {
             startActivity(new Intent("android.settings.MANAGE_ALL_FILES_ACCESS_PERMISSION"));
-        } else if (i <= this.mFileList.size()) {
-            int downNewFile = this.mActivity.downNewFile(this.mFileList.get(i));
+        } else if (position < this.mFileList.size()) {
+            int downNewFile = this.mActivity.downNewFile(this.mFileList.get(position));
             if (1 == downNewFile) {
                 toastSf((int) R.string.file_is_downed);
             } else if (2 == downNewFile) {
@@ -109,11 +109,11 @@ public class PlayFragmentFile extends FragmentBase implements View.OnClickListen
     }
 
     public class FileAdapter extends BaseAdapter {
-        public Object getItem(int i) {
+        public Object getItem(int position) {
             return null;
         }
 
-        public long getItemId(int i) {
+        public long getItemId(int position) {
             return 0;
         }
 
@@ -127,33 +127,32 @@ public class PlayFragmentFile extends FragmentBase implements View.OnClickListen
             return PlayFragmentFile.this.mFileList.size();
         }
 
-        public View getView(int i, View view, ViewGroup viewGroup) {
+        public View getView(int position, View convertView, ViewGroup parent) {
             FileTag fileTag;
-            if (view == null) {
-                FileTag fileTag2 = new FileTag();
+            if (convertView == null) {
+                FileTag newFileTag = new FileTag();
                 View inflate = PlayFragmentFile.this.mInflater.inflate(R.layout.play_file_list_item, (ViewGroup) null);
-                fileTag2.ivIcon = (ImageView) inflate.findViewById(R.id.icon_view);
-                fileTag2.tvTitle = (TextView) inflate.findViewById(R.id.title_view);
-                fileTag2.tvSubTitle = (TextView) inflate.findViewById(R.id.subtitle_view);
-                fileTag2.ivOpt = (ImageView) inflate.findViewById(R.id.icon_opt);
-                fileTag2.ivOpt.setOnClickListener(new View.OnClickListener() {
+                newFileTag.ivIcon = (ImageView) inflate.findViewById(R.id.icon_view);
+                newFileTag.tvTitle = (TextView) inflate.findViewById(R.id.title_view);
+                newFileTag.tvSubTitle = (TextView) inflate.findViewById(R.id.subtitle_view);
+                newFileTag.ivOpt = (ImageView) inflate.findViewById(R.id.icon_opt);
+                newFileTag.ivOpt.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View view) {
                         PlayFragmentFile.this.onFileDownClick(((Integer) view.getTag()).intValue());
                     }
                 });
-                fileTag2.ivOpt.setTag(Integer.valueOf(i));
-                inflate.setTag(fileTag2);
-                View view2 = inflate;
-                fileTag = fileTag2;
-                view = view2;
+                newFileTag.ivOpt.setTag(Integer.valueOf(position));
+                inflate.setTag(newFileTag);
+                fileTag = newFileTag;
+                convertView = inflate;
             } else {
-                fileTag = (FileTag) view.getTag();
-                fileTag.ivOpt.setTag(Integer.valueOf(i));
+                fileTag = (FileTag) convertView.getTag();
+                fileTag.ivOpt.setTag(Integer.valueOf(position));
             }
-            view.setBackgroundColor(PlayFragmentFile.this.mFileList.get(i).mIsPlay ? Color.rgb(226, 241, Type.TKEY) : Color.rgb(255, 255, 255));
-            fileTag.tvTitle.setText(PlayFragmentFile.this.mFileList.get(i).mTitle);
-            fileTag.tvSubTitle.setText(PlayFragmentFile.this.mFileList.get(i).mSubTitle);
-            return view;
+            convertView.setBackgroundColor(PlayFragmentFile.this.mFileList.get(position).mIsPlay ? Color.rgb(226, 241, Type.TKEY) : Color.rgb(255, 255, 255));
+            fileTag.tvTitle.setText(PlayFragmentFile.this.mFileList.get(position).mTitle);
+            fileTag.tvSubTitle.setText(PlayFragmentFile.this.mFileList.get(position).mSubTitle);
+            return convertView;
         }
     }
 }
