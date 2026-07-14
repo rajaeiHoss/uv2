40a35dc57d16d6c950078cdfeb77bec9b1212111
hossien <h.rajaei@vasl.ir>
2026-07-14 11:17:55 +0330

    Clarify ToastSf message parameters

--- decompiled/sources/com/streamax/utils/ToastSf.java
+++ decompiled/sources/com/streamax/utils/ToastSf.java
@@ -4,20 +4,20 @@ import android.app.Activity;
 import android.widget.Toast;
 
 public class ToastSf {
-    public static void toastSf(Activity activity, final String str) {
+    public static void toastSf(Activity activity, final String message) {
         if ("main".equalsIgnoreCase(Thread.currentThread().getName())) {
-            Toast.makeText(AppProxy.getContext(), str, 0).show();
+            Toast.makeText(AppProxy.getContext(), message, 0).show();
         } else {
             activity.runOnUiThread(new Runnable() {
                 public void run() {
-                    Toast.makeText(AppProxy.getContext(), str, 0).show();
+                    Toast.makeText(AppProxy.getContext(), message, 0).show();
                 }
             });
         }
     }
 
-    public static void toastSf(Activity activity, int i) {
-        final String string = StringUtils.getString(Integer.valueOf(i));
+    public static void toastSf(Activity activity, int messageResId) {
+        final String string = StringUtils.getString(Integer.valueOf(messageResId));
         if ("main".equalsIgnoreCase(Thread.currentThread().getName())) {
             Toast.makeText(AppProxy.getContext(), string, 0).show();
         } else {
