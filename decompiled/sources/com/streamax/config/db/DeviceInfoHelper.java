d19aa5d08e28d8805c1ddb5fc92430a2df78cc37
hossien <h.rajaei@vasl.ir>
2026-07-14 11:35:36 +0330

    Clarify DeviceInfoHelper lifecycle parameters

--- decompiled/sources/com/streamax/config/db/DeviceInfoHelper.java
+++ decompiled/sources/com/streamax/config/db/DeviceInfoHelper.java
@@ -7,14 +7,14 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DeviceInfoHelper extends SQLiteOpenHelper {
     public Context mContext;
 
-    public void onUpgrade(SQLiteDatabase sQLiteDatabase, int i, int i2) {
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
     }
 
     public DeviceInfoHelper(Context context) {
         super(context, DatabaseConfig.DatabaseName, (SQLiteDatabase.CursorFactory) null, 1);
     }
 
-    public void onCreate(SQLiteDatabase sQLiteDatabase) {
-        sQLiteDatabase.execSQL("Create table DeviceInfo(id integer primary key autoincrement,dbDeviceIp varchar,dbCurrentChannel Integer,dbCurrentDay Integer,dbCurrentPlan Integer,dbCurrentTotalChannel Integer,dbCurrentTotalPlan Integer);");
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL("Create table DeviceInfo(id integer primary key autoincrement,dbDeviceIp varchar,dbCurrentChannel Integer,dbCurrentDay Integer,dbCurrentPlan Integer,dbCurrentTotalChannel Integer,dbCurrentTotalPlan Integer);");
     }
 }
